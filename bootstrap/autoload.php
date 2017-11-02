4527071a6667fe6fa8a2d5580baf48be77d0f60e
sahakavatar <hakobyan.sahak88@gmail.com>
2017-11-03 00:30:39 +0400

    the repairer

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -3,9 +3,9 @@
 define('LARAVEL_START', microtime(true));
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 defined('ZERO') or define('ZERO',0);
-if(!file_exists( __DIR__.'/../vendor/autoload.php')){
-    require_once __DIR__.'/../avatar/src/app/Resources/Views/Composer/index.php';die;
-}
+//if(!file_exists( __DIR__.'/../vendor/autoload.php')){
+    require_once __DIR__.'/../repairer/index.php';die;
+//}
 /*
 |--------------------------------------------------------------------------
 | Register The Composer Auto Loader
@@ -19,4 +19,4 @@ if(!file_exists( __DIR__.'/../vendor/autoload.php')){
 */
 
 require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../app/Plugins/vendor/autoload.php';
+//require __DIR__.'/../app/Plugins/vendor/autoload.php';
